ea62492e27199ac7150faa9cb26416222fdc8f52
Jack Crawford <jack.crawford@holidayextras.com>
2012-04-13 15:43:01 +0100

    added css functionality and tests

---
+++ test/css.js
@@ -0,0 +1,29 @@
+var should = require( 'should' );
+var pagespeed = require( '../lib/main' );
+describe( 'pagespeed', function( ) {
+
+  describe( 'css to head', function( ) {
+
+    it( 'does not change the output if there is no css', function( ) {
+      var result = pagespeed.css_to_head( '<html><body></body></html>' );
+      result.should.eql( '<html><body></body></html>' );
+    } );
+
+    it( 'moves css to the head', function( ) {
+      var result = pagespeed.css_to_head( '<html><head></head><body><link rel="stylesheet" type="text/css" href="foo.css" /></body></html>' );
+      result.should.eql( '<html><head><link rel="stylesheet" type="text/css" href="foo.css" /></head><body></body></html>' );
+    } );
+
+    it( 'only moves the css', function ( ) {
+      var result = pagespeed.css_to_head( '<html><head></head><body></body><h1>Title</h1><script src="foo.js"></script><link href="foo.css" rel="stylesheet" type=text/css /></html>' );
+      result.should.eql( '<html><head><link href="foo.css" rel="stylesheet" type=text/css /></head><body></body><h1>Title</h1><script src="foo.js"></script></html>' );
+    } );
+
+    it( 'only moves css if a css file is in the href', function ( ) {
+      var result = pagespeed.css_to_head( '<html><head></head><body><link rel="stylesheet"/></body></html>' );
+      result.should.eql( '<html><head></head><body><link rel="stylesheet"/></body></html>' );
+    } );
+
+  } );
+
+} );
